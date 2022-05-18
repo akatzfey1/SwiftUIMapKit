8af2714976f5e086e308b87e43a325e3db0eb7ad
akatzfey <katzfeyalex@gmail.com>
2022-05-18 16:34:40 -0700

    Initial Commit

---
+++ SwiftUI MapKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI MapKit
+//
+//  Created by Alexander Katzfey on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI MapKit/SwiftUI_MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapKitApp.swift
+//  SwiftUI MapKit
+//
+//  Created by Alexander Katzfey on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
